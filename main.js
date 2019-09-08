f8104266354a016866618312e41aef5e6e36a740
Leinad <43974923+dabass2@users.noreply.github.com>
2019-09-08 17:10:34 -0500

    Updated Files

--- main.js
+++ main.js
@@ -1,79 +1,52 @@
-var items = []
-var t
-var mercX, mercY
-var venusX, venusY
-var earthX, earthY
-var marsX, marsY
-var jupiterX, jupiterY
-var saturnX, saturnY
-var uranusX, uranusY
-var neptuneX, neptuneY
-let r = 40
+let img;
 let theta = 0
-let step = 180
+// let font;
 
-function setup()
-{
-  createCanvas(windowWidth, windowHeight)
+function preload() {
+  // font = loadFont('./Roboto-Black.ttf');
+  earth = loadImage('./earth.jpg');
+  moon = loadImage('./moon2.jpg')
 }
 
-function draw()
-{
-  noStroke()
-  background(51)
-  fill("yellow")
-  translate(width/2, height/2)
-  circle(0, 0, 20)
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  // textFont(font)
+  textSize(width/50)
+  textAlign(CENTER, CENTER)
+}
 
-  t = jQuery.getJSON('./planetData.json')
+function draw() {
+  orbitControl()
+  background(0);
+  ambientLight(250);
 
-  t.then(function(results){
-    // console.log(results)
-    r = .02 * windowWidth
-    mercDFS = results.planets.mercury.distancefromsun
-    mercX = (r * cos(theta) * int(results.planets.mercury.orbitalvelocity)) / 100
-    mercY = (r * sin(theta) * int(results.planets.mercury.orbitalvelocity)) / 100
-
-    r = .04 * windowWidth
-    venusDFS = results.planets.venus.distancefromsun
-    venusX = ((r * cos(theta) * int(results.planets.venus.orbitalvelocity)) / 100) * (1/venusDFS)
-    venusY = ((r * sin(theta) * int(results.planets.venus.orbitalvelocity)) / 100) * (1/venusDFS)
-
-    r = .07 * windowWidth
-    earthDFS = results.planets.earth.distancefromsun
-    earthX = r * cos(theta+int(results.planets.earth.orbitalvelocity))
-    earthY = r * sin(theta+int(results.planets.earth.orbitalvelocity))
-
-    // marsDFS = results.planets.mars.distancefromsun
-    // marsX = ((marsDFS/windowWidth) * cos(theta))
-    // marsY = ((marsDFS/windowWidth) * sin(theta))
-
-    r = .3 * windowWidth
-    jupiterDFS = results.planets.jupiter.distancefromsun
-    jupiterX = r * cos(theta/results.planets.jupiter.orbitalvelocity)
-    jupiterY = r * sin(theta/results.planets.jupiter.orbitalvelocity)
-  })
-
-  fill("gray")
-  ellipse(mercX, mercY, 10)
-
-  fill("orange")
-  ellipse(venusX, venusY, 10)
-
-  fill("blue")
-  ellipse(earthX, earthY, 10)
-
-  // fill("red")
-  // ellipse(marsX, marsY, 10)
-
-  noFill()
-  stroke("brown")
-  circle(0, 0, 500)
+  stroke('red')
+  line(-windowWidth, 0, windowWidth, 0) // x-axis
+  stroke('blue')
+  line(0, -windowHeight, 0, windowHeight) // y-axis
+  stroke('green')
+  line(0, 0, -windowWidth, 0, 0, windowWidth) // z-axis
   noStroke()
 
-  fill("white")
-  ellipse(jupiterX, jupiterY, 40)
+  angleMode(DEGREES)
 
-  theta += TWO_PI/360
+  push()
+  rotateY(frameCount * 0.289)
+  rotate(23.4)
+  // rotate(23.4, [0, 1, 0])
+  texture(earth)
+  sphere(windowWidth*.1, 48, 32)
+  pop()
 
+  push()
+  rotateY(frameCount * 0.273)
+  rotate(6.68)
+  texture(moon)
+  r = windowWidth*.4
+  translate(-r*cos(theta) , -((r*(tan(5.14)))), r*sin(theta))
+  // console.log(-((r*(tan(5.14)))))
+  sphere(50, 48, 32)
+  pop()
+
+  theta += 1
 }
